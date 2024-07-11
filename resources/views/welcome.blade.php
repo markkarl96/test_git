7bad3327f6db8356c4f0d41da771765e5fdb3b2e
Vik <my@mail.com>
2024-07-11 21:28:40 +0500

    change h2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Документашка</h2>
                                 
                             </div>
 
